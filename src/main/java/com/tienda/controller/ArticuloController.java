80cadcf641e46dd7ac1f0a1d533390c0f7876078
jpicados <98858767+jpicados@users.noreply.github.com>
2023-04-16 12:06:30 -0600

    Todas las tablas completas del proyeto

---
+++ src/main/java/com/tienda/controller/ArticuloController.java
@@ -0,0 +1,51 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ArticuloDao;
+import com.tienda.domain.Articulo;
+import com.tienda.service.ArticuloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articulo")
+public class ArticuloController {
+    @Autowired
+    ArticuloService articuloService;   
+     
+     
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+     //   var articulos = articuloService.getArticulos(false);
+     var articulos = articuloService.getArticulos(false);
+        model.addAttribute("articulos", articulos);
+        model.addAttribute("totalArticulos",articulos.size());        
+        return "/articulo/listado";
+    }
+
+    @GetMapping("/eliminar/{idArticulo}")
+    public String eliminaArticulo(Articulo articulo) {
+        articuloService.deleteArticulo(articulo);
+        return "redirect:/articulo/listado";
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevoArticulo(Articulo articulo) {
+        return "/articulo/modifica";
+    }
+    @PostMapping("/guardar")
+    public String guardaArticulo(Articulo articulo) {
+        articuloService.saveArticulo(articulo);
+        return "redirect:/articulo/listado";
+    }
+
+    @GetMapping("/modificar/{idArticulo}")
+    public String modificaArticulo(Articulo articulo, Model model) {
+        articulo = articuloService.getArticulo(articulo);
+        model.addAttribute("articulo", articulo);
+        return "/articulo/modifica";
+    }
+}
